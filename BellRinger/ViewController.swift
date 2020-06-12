f9311fa1af90b1aec90b6658d024d0ed5e4f0b44
Chad Parker <chad@chadparker.com>
2020-06-11 19:48:42 -0700

    Initial Commit

---
+++ BellRinger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BellRinger
+//
+//  Created by Chad Parker on 6/11/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
